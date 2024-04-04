8f54ec6b77f00dea9b4b5007625ba493842788ea
Balvorn <leo-marcou@hotmail.fr>
2024-04-04 18:27:26 +0200

    +home page

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,36 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("alt", name)
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const location = document.createElement('p');
+        location.innerHTML = city + ", " + country
+        location.setAttribute("class", "location")
+
+        const tag = document.createElement('p');
+        tag.innerHTML = tagline
+        tag.setAttribute("class", "tag")
+
+        const rate = document.createElement('p');
+        rate.innerHTML = price + "€/jour"
+        rate.setAttribute("class", "rate")
+
+        const link = document.createElement('a');
+        link.setAttribute("href", "photographer.html?id=" + id)
+        link.append(img, h2);
+
+        const article = document.createElement('article');
+        article.append(link, location, tag, rate)
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
